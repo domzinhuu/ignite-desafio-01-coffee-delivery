f8665b2d359a5defef01917816469c2900fff28f
Maique Rosa <maique.rosa@gmail.com>
2023-05-30 20:20:20 -0300

    chore: estruturando projeto com assets e styles

---
+++ src/styles/themes/global.ts
@@ -0,0 +1,97 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+    
+    body{
+        background: ${(props) => props.theme.background};
+    }
+
+    body, input, textarea, button{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    .title-xl{
+        font-family: 'Baloo 2', sans-serif;
+        font-weight: bolder;
+        line-height: 130%;
+        font-size: 3rem;
+    }
+
+    .title-l{
+        font-family: 'Baloo 2', sans-serif;
+        font-weight: bolder;
+        line-height: 130%;
+        font-size: 2rem;
+    }
+
+    .title-m{
+        font-family: 'Baloo 2', sans-serif;
+        font-weight: bolder;
+        line-height: 130%;
+        font-size: 1.5rem;
+    }
+
+    .title-s{
+        font-family: 'Baloo 2', sans-serif;
+        font-weight: bold;
+        line-height: 130%;
+        font-size: 1.25rem;
+    }
+
+    .title-xs{
+        font-family: 'Baloo 2', sans-serif;
+        font-weight: bold;
+        line-height: 130%;
+        font-size: 1.125rem;
+    }
+
+    .text-l{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        line-height: 130%;
+        font-size: 1.25rem;
+    }
+    
+    .text-l-bold{
+        font-family: 'Roboto', sans-serif;
+        font-weight: bold;
+        line-height: 130%;
+        font-size: 1.25rem;
+    }
+
+    .text-m{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        line-height: 130%;
+        font-size: 1rem;
+    }
+
+    .text-m-bold{
+        font-family: 'Roboto', sans-serif;
+        font-weight: bold;
+        line-height: 130%;
+        font-size: 1rem;
+    }
+
+    .text-s{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        line-height: 130%;
+        font-size: 0.875rem;
+    }
+
+    .text-xs{
+        font-family: 'Roboto', sans-serif;
+        font-weight: bold;
+        line-height: 130%;
+        font-size: 0.75rem;
+    }
+`
